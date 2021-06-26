b7a1934dcf83a71782023042390435c3791ded06
Lo Hsin <steveluoxin@gmail.com>
2021-06-26 17:12:13 +0800

    test

---
+++ js/test.js
@@ -0,0 +1,32 @@
+{
+    let title = "Everything That's New in the iOS 15 FaceTime App: SharePlay, Android Support, Spatial Audio and More"
+    let content = `
+    Apple today announced it is offering an extended one-month free trial for the Paramount+ streaming service to new users who subscribe within the Apple TV app through June 30, &lt;a href="https://www.macobserver.com/tips/quick-tip/us-users-paramount-plus-free-month-apple-tv/"&gt;as spotted by &lt;em&gt;The Mac Observer&lt;/em&gt;&lt;/a&gt;. The offer is valid in the U.S. only.
+    &lt;br/&gt;
+    
+    &lt;br/&gt;
+    &lt;img src="https://images.macrumors.com/article-new/2021/06/paramount-plus-apple-tv-app.jpg" alt="" width="1600" height="900" class="aligncenter size-full wp-image-805269" /&gt;
+    &lt;br/&gt;
+    Previously known as CBS All Access, Paramount+ provides access to TV shows and movies from ViacomCBS and its various subsidiaries, including CBS, Paramount Pictures, Nickelodeon, MTV, Comedy Central, BET, and others. The streaming service typically offers a seven-day free trial to new users when subscribing directly.
+    &lt;br/&gt;
+    
+    &lt;br/&gt;
+    Paramount+ is one of several "Apple TV channels," meaning that users can subscribe to and watch the streaming service in the Apple TV app. Users can also download the &lt;a href="https://apps.apple.com/us/app/paramount/id1340650234"&gt;Paramount+ app&lt;/a&gt; for iPhone, iPad, and Apple TV.
+    &lt;br/&gt;
+    
+    &lt;br/&gt;
+    &lt;div class="center-wrap"&gt;&lt;blockquote class="twitter-tweet"&gt;&lt;p lang="en" dir="ltr"&gt;US only.&lt;/p&gt;&amp;mdash; Apple TV (@AppleTV) &lt;a href="https://twitter.com/AppleTV/status/1407025255500054532?ref_src=twsrc%5Etfw"&gt;June 21, 2021&lt;/a&gt;&lt;/blockquote&gt; &lt;script async src="https://platform.twitter.com/widgets.js" charset="utf-8"&gt;&lt;/script&gt;&lt;/div&gt;
+    &lt;br/&gt;
+    Paramount+ costs &amp;#36;4.99 per month with ads, or &amp;#36;9.99 per month without ads.&lt;div class="linkback"&gt;Related Roundup: &lt;a href="https://www.macrumors.com/roundup/apple-tv/"&gt;Apple TV&lt;/a&gt;&lt;/div&gt;&lt;div class="linkback"&gt;Tag: &lt;a href="https://www.macrumors.com/guide/apple-tv-channels/"&gt;Apple TV Channels&lt;/a&gt;&lt;/div&gt;&lt;div class="linkback"&gt;Buyer's Guide: &lt;a href="https://buyersguide.macrumors.com/#Apple_TV"&gt;Apple TV (Buy Now)&lt;/a&gt;&lt;/div&gt;&lt;div class="linkback"&gt;Related Forum: &lt;a href="https://forums.macrumors.com/forums/apple-tv-and-home-theater.100"&gt;Apple TV and Home Theater&lt;/a&gt;&lt;/div&gt;&lt;br/&gt;This article, &amp;quot;&lt;a href="https://www.macrumors.com/2021/06/21/apple-tv-app-one-month-paramount-plus-trial/"&gt;Apple TV App Offering Extended One-Month Paramount+ Trial Through June&lt;/a&gt;&amp;quot; first appeared on &lt;a href="https://www.macrumors.com"&gt;MacRumors.com&lt;/a&gt;&lt;br/&gt;&lt;br/&gt;&lt;a href="https://forums.macrumors.com/threads/apple-tv-app-offering-extended-one-month-paramount-trial-through-june.2301766/"&gt;Discuss this article&lt;/a&gt; in our forums&lt;br/&gt;&lt;br/&gt;&lt;div class="feedflare"&gt;
+    &lt;a href="http://feeds.macrumors.com/~ff/MacRumors-All?a=gdKBQfio6-s:n9s2CFbF-VM:6W8y8wAjSf4"&gt;&lt;img src="http://feeds.feedburner.com/~ff/MacRumors-All?d=6W8y8wAjSf4" border="0"&gt;&lt;/img&gt;&lt;/a&gt; &lt;a href="http://feeds.macrumors.com/~ff/MacRumors-All?a=gdKBQfio6-s:n9s2CFbF-VM:qj6IDK7rITs"&gt;&lt;img src="http://feeds.feedburner.com/~ff/MacRumors-All?d=qj6IDK7rITs" border="0"&gt;&lt;/img&gt;&lt;/a&gt;
+    &lt;/div&gt;&lt;img src="http://feeds.feedburner.com/~r/MacRumors-All/~4/gdKBQfio6-s" height="1" width="1" alt=""/&gt;
+    `
+
+    var changePost = function() {
+        const newPost = {
+            title: title,
+            content: content
+        }
+        vm.changePost(newPost)
+    }
+}
